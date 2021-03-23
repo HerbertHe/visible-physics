0190a56a1e83dbf17a62aeeae25a3b815c278126
HerbertHe <30955264+HerbertHe@users.noreply.github.com>
2021-03-23 12:45:03 +0000

    Deploying to gh-pages from @ HerbertHe/visible-physics@33669c4d6e5221595352493f517e07a0808ed766 🚀

--- interference-of-light/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 波的叠加原理可视化
- */
-export declare const PrincipleOfWaveSuperimposing: () => JSX.Element;
-declare const _default: ({ title }: {
-    title: string;
-}) => JSX.Element;
-export default _default;
